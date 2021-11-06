3c3e4ab94d155b7faee2579a9b11972087353c3b
Daniil <dskotrov@miem.hse.ru>
2021-11-06 19:36:55 +0300

    made a lot of function for options in menu

--- main.py
+++ main.py
@@ -24,33 +24,62 @@ def welcome_message(message):
 
 @bot.message_handler(commands=['menu'])
 def menu(message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(text="О Сквоте", callback_data="squat_button"))
-    markup.add(types.InlineKeyboardButton(text="Связаться с сотрудником", callback_data="contact_button"))
-    markup.add(types.InlineKeyboardButton(text="Забронировать столик", callback_data="reserve_button"))
-    markup.add(types.InlineKeyboardButton(text="Меню", callback_data="menu_button"))
+    menu_markup = types.InlineKeyboardMarkup()
+    menu_markup.add(types.InlineKeyboardButton(text="О Сквоте", callback_data="squat_button"))
+    menu_markup.add(types.InlineKeyboardButton(text="Связаться с сотрудником", callback_data="contact_button"))
+    menu_markup.add(types.InlineKeyboardButton(text="Забронировать столик", callback_data="reserve_button"))
+    menu_markup.add(types.InlineKeyboardButton(text="Меню", callback_data="menu_button"))
 
-    bot.send_message(message.chat.id, "Выберите действие: ", reply_markup=markup)
+    bot.send_message(message.chat.id, "Выберите действие: ", reply_markup=menu_markup)
+
+
+def condition_yes(message):
+    bot.send_message(message.chat.id, "yes bla bla")
+
+
+def condition_no(message):
+    bot.send_message(message.chat.id, "bla bla no")
 
 
 def squat(message):
+    condition_markup = types.InlineKeyboardMarkup()
+    condition_markup.add(types.InlineKeyboardButton(text="Да", callback_data="cond_yes"))
+    condition_markup.add(types.InlineKeyboardButton(text="Нет", callback_data="cond_no"))
+
     bot.send_message(message.chat.id, 'Привет, и добро пожаловать в "Сквот"')
+    time.sleep(1)
+    bot.send_message(message.chat.id, "Многие наверное и не знают, что обозначает данное слово, но у него есть своя "
+                                      "богатая история, которая берет корни еще с 1950-х годов.")
+    time.sleep(1)
+    bot.send_message(message.chat.id, "Узнать дальше?", reply_markup=condition_markup)
+
+
+def contact(message):
+    bot.send_message(message.chat.id, "Напишите @razrabot")
+
+
+def reserve(message):
+    bot.send_message(message.chat.id, "На какое имя забронировать?")
+
+
+def menu_picture(message):
+    bot.send_message(message.chat.id, "Меню")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
-    answer = "0"
-
     if call.data == "squat_button":
         squat(call.message)
     elif call.data == "contact_button":
-        answer = 'Напишите @razrabot'
+        contact(call.message)
     elif call.data == 'reserve_button':
-        answer = 'На какое имя забронировать?'
+        reserve(call.message)
     elif call.data == 'menu_button':
-        answer = 'Меню'
-
-    bot.send_message(call.message.chat.id, answer)
+        menu_picture(call.message)
+    elif call.data == 'cond_yes':
+        condition_yes(call.message)
+    elif call.data == 'cond_no':
+        condition_no(call.message)
 
 
 bot.infinity_polling()
